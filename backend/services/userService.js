76f3806957ce17465ee365d526df6b16c03febc5
SwayamSalvi2005 <swayamnsalvi@gmail.com>
2026-07-15 03:28:55 +0530

    added updateService and deleteService for user

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -101,3 +101,126 @@ export const loginService = async ({email, password}) =>{
     // return the token and user, if he is authenticat
     return sendAuthResponse(user);
 }
+
+
+
+// 3. UPDATE USER SERVICE
+export const updateUserService = async(userID, {name, email}) =>{
+    
+    //if credentials does not exists throw error
+    if (!name || !email) {
+        throw new Error("Please enter name and email");
+    }
+    
+    //check email format
+    if (!validator.isEmail(email)) {
+        throw new Error("Invalid email format");
+    }
+
+    //clean emai
+    const normalEmail = email.toLowerCase().trim();
+
+    // check if email is already taken
+      /// $ne = not equal
+    const existingUser = await User.findOne({email: normalEmail, _id: { $ne: userID} });
+
+    // if user exists, email is alredy taken
+    if (existingUser) {
+        throw new Error("Email already in use");
+    }
+
+    // if email not taken update it 
+    const updatedUser = await User.findOneAndUpdate(
+        {_id: userID},
+        {
+            name: name.trim(),
+            email: normalEmail
+        },{
+            new: true, // give the updated document
+            runValidators: true // run validation on this
+        }
+    );
+
+    return updatedUser;
+}
+
+
+
+// 4. UPDATE PASSWORD SERVICE
+export const updatePasswordService = async (userId, { currentPassword, newPassword }) => {
+
+    //if credentails not found, throw error
+    if (!currentPassword || !newPassword) {
+        throw new Error("Please provide both current and new password");
+    }
+
+    // validate new password 
+    if (!validator.isStrongPassword(newPassword, {
+        minLength: 8,
+        minLowercase: 0,
+        minUppercase: 0,
+        minNumbers: 0,
+        minSymbols: 0
+    })) {
+        throw new Error("Password should atleast 8 characters and contain at least one uppercase letter, one lowercase letter, one number, and one special character");
+    }
+
+    // get user with old password
+    const user = await User.findById(userId).select("+password");
+
+    // check the current pass entered by user is correct?
+    const isPasswordValid = await user.comparePassword(currentPassword);
+
+    // if not correct
+    if (!isPasswordValid) {
+        throw new Error("Current password is incorrect");
+    }
+
+    // uf original password is correct, updateit with the new pass
+    // update password ... pre-save hook(userModel) will hash password
+    user.password = newPassword;
+    await user.save();
+
+    return sendAuthResponse(user);
+};
+
+
+
+
+// 5. DELETE USER SERVICE
+export const deleteUserService = async(userID, {currentPassword, confirmMessage}) =>{
+
+    // if password not entered
+    if(!currentPassword){
+        throw new Error('Please provide password')
+    }
+
+    // if confim message not entered
+    if(confirmMessage !== 'DELETE'){
+        throw new Error('Type "DELETE" to continue')
+    }
+
+    // get the user with that id and password
+    const user = await User.findById(userID).select("+password");
+
+    //if user not exits through error
+    if(!user){
+        throw new Error('User not found')
+    }
+    
+    // compare the entered currett password
+    const isPasswordValid = await user.comparePassword(currentPassword);
+
+    //if pass invalid through error
+    if(!isPasswordValid){
+        throw new Error('Incorrect password')
+    }
+
+    // Delete all tasks belonging to this user first
+    await Task.deleteMany({ owner: userID });
+
+    // delete the user
+    await User.findByIdAndDelete(userID);
+
+    return;
+}
